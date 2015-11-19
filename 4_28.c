f7f5d26dfe4ac25aa9b0d3e2a7e0e7ff2be4e0c8
scensorECHO <xxartofwarxx@gmail.com>
2015-11-18 22:10:56 -0500

    Started switch array filler

--- 4_28.c
+++ 4_28.c
@@ -7,24 +7,56 @@ int main(){
 
 	int paycode;
 	int payroll [] = {0,0,0,0};
-	int counter = 0;
+	int c = 0; // counter for list
+	int f = 0; // counter for fields in list
 	
 	char*** employees;
 	// 3d array contains strings, list of info, and list of lists
 
 	printf("Please enter the employee pay code: ");
-	scanf("%d", &paycode);
+	scanf("%c", &paycode);
+
+	/* I'm setting up my program to hold a 2D array of strings
+	 * (char[][][] or char***) in order to hold given values for
+	 * each inputted employee. 
+	 * manager:   	{type, name, salary}
+	 * hourly:    	{type, name, hours, wage, total}
+	 * commission:	{type, name, sales, total}
+	 * pieceworker:	{type, name, produced, price, total}
+	 */
 
 	while(paycode!=-1){
+		f=0; // reset field counter
 		switch(paycode){
 			case 1: // manager
-				float salary;
-				printf("How much is your weekly salary? (X.XX)");
-				scanf("%f", &salary);
-
+				employees[c][f++]=paycode;
+				printf("Enter your full name: ");
+				scanf("%s", &employees[c][f++]);
+				printf("How much is your weekly salary? (X.XX): ");
+				scanf("%s", &employees[c][f]);
+				
 			case 2: // hourly
+				employees[c][f++]=paycode;
+				printf("Enter your full name: ");
+				scanf("%s", &employees[c][f++]);
+				printf("Enter the number of hours you worked (integer): ");
+				scanf("%s", &employees[c][f++]);
+				printf("Enter your hourly wage: (X.XX)");
+				scanf("%s", &employees[c][f++]);
+
 			case 3: // commission
+				employees[c][f++]=paycode;
+				printf("Enter your full name: ");
+				scanf("%s", &employees[c][f++]);
+				printf("Enter your total sales: ");
+				scanf("%s", &employees[c][f++]);
+
+
 			case 4: // pieceworker
+				employees[c][f++]=paycode;
+				printf("Enter your full name: ");
+				scanf("%s", &employees[c][f++]);
+				
 		}
 	}
 } 
