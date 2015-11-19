40fe7390d8d38378d8d3d0bdba2889dd5b6d95cf
scensorECHO <xxartofwarxx@gmail.com>
2015-11-19 14:01:05 -0500

    Updated to struct

--- 4_28.c
+++ 4_28.c
@@ -10,21 +10,26 @@ void promptCode();
 
 // globals
 const char* codestr = "Please enter the employee pay code (-1 to end employee input): ";
+typedef struct {
+	int paycode;
+	char* name;
+	float total;
+} Employee;
+
 
 int main(){
 
 	int paycode;
-	int payroll [] = {0,0,0,0};
-	int c = 0; // counter for list
-	int f = 0; // counter for fields in list
-	int len = 10; // size of array - starts at 10
+	Employee *payroll;
+	int c = 0; // counter for array
+	int len = 0; // length of array
 
 	// standard strings
 	const char* fname = "Enter employee last name: ";
-	const char* totalPaycheck = "%s's total paycheck is $%s\n\n";
+	const char* totalPaycheck = "%s's total paycheck is $%f\n\n";
 	
-	const size_t BUFFER = 18; // buffer for parsing float/int
-	const size_t BUF_CODE = 1; // buffer for paycode - always 1
+	// const size_t BUFFER = 18; // buffer for parsing float/int
+	// const size_t BUF_CODE = 1; // buffer for paycode - always 1
 
 	// parse values for arithmetic in switch
 	int hours;
@@ -34,10 +39,6 @@ int main(){
 	int quantity;
 	float price;
 	
-	// 3d array contains strings > list of info > list of lists
-	char emp[len][5][24];// = (char*) malloc(5*len*sizeof(char)*24);
-	memset( emp, 0, 5*len*sizeof(char)*24);
-
 	promptCode();
 	scanf("%d", &paycode);
 
@@ -52,54 +53,30 @@ int main(){
 	 */
 
 	while(paycode!=-1){
-		f=0; // reset field counter
-		
-		// Test if array limit reached and increase size by 10
-		if(len<=(c+1)){
-			len+=10;
-			//emp = realloc(emp, 5*len*sizeof(char)*24);
-			char tempE[len][5][24];// = malloc(emp,5*len*sizeof(char)*24);
-			memset(tempE,0,5*len*sizeof(char)*24);
-			for(int x=0; x<c; x++){
-				for(int r=0; r<5; r++){
-					for(int s=0; s<strlen(emp[r][x]); s++){
-						tempE[r][x][s]=emp[r][x][s];
-					}
-				}
-			}
-			// zero array and set to new size
-			memset(emp,0,5*len*sizeof(char)*24);
-			//emp = tempE;
-			for(int x=0; x<c; x++){
-				for(int r=0; r<5; r++){
-					for(int s=0; s<strlen(emp[r][x]); s++){
-						emp[r][x][s]=tempE[r][x][s];
-					}
-				}
-			}
-		}
-
 		// Check user input and respond
 		switch(paycode){
+			Employee emp;
 			case 1: // manager
 				// Set employees pay code
-				snprintf(emp[c][f++],BUF_CODE,"%d",paycode);
+				emp.paycode=paycode;
 				
 				// Prompt for user First name
 				printf(fname);
-				scanf("%s", &emp[c][f++]);
+				scanf("%s", &emp.name);
 
 				// Manager specific: Get weekly salary
 				printf("How much is their weekly salary? (X.XX): ");
-				scanf("%s", &emp[c][f]);
+				scanf("%s", &emp.total);
 
 				// Display paycheck to user
-				printf(totalPaycheck,emp[c][1],emp[c][2]);
-				c++;
+				printf(totalPaycheck,emp.name,emp.total);
+
+				// Add Employee to array
+				addToArray(emp);
 				break;
 			case 2: // hourly
 				// Set employees pay code
-				snprintf(emp[c][f++],BUF_CODE,"%d",paycode);
+				emp.name=paycode;
 				
 				// Prompt for user First name
 				printf(fname);
@@ -108,21 +85,21 @@ int main(){
 				// Get hours worked 
 				printf("Enter the number of hours worked (integer): ");
 				scanf("%d", &hours);
-				snprintf(emp[c][f++],BUFFER,"%d",hours);
 				
 				// Get hourly wage
 				printf("Enter hourly wage (X.XX):");
 				scanf("%f", &wage);
-				snprintf(emp[c][f++],BUFFER,"%.2f",wage);
 			
 				// Calculate total and display to user
-				snprintf(emp[c][f],BUFFER,"%.2f",wage*respHrs(hours));
-				printf(totalPaycheck,emp[c][1],emp[c][4]);
-				c++;
+				emp.total=wage*respHrs(hours);
+				printf(totalPaycheck,emp.name,emp.total);
+
+				// Add Employee to array
+				addToArray(emp);
 				break;
 			case 3: // commission
 				// Set employees pay code
-				snprintf(emp[c][f++],BUF_CODE,"%d",paycode);
+				emp.paycode=paycode;
 				
 				// Prompt for user First name
 				printf(fname);
@@ -131,16 +108,17 @@ int main(){
 				// Get total sales 
 				printf("Enter total sales (X.XX):");
 				scanf("%f", &sales);
-				snprintf(emp[c][f++],BUFFER,"%.2f",sales);
 				
 				// Calculate total and display to user
-				snprintf(emp[c][f],BUFFER,"%.2f",sales+250.00);
-				printf(totalPaycheck,emp[c][1],emp[c][3]);
-				c++;
+				emp.total=sales+250.00;
+				printf(totalPaycheck,emp.name,emp.total);
+
+				// Add Employee to array
+				addToArray(emp);
 				break;
 			case 4: // pieceworker
 				// Set employees pay code
-				snprintf(emp[c][f++],BUF_CODE,"%d",paycode);
+				emp.paycode=paycode;
 				
 				// Prompt for user First name
 				printf(fname);
@@ -149,19 +127,19 @@ int main(){
 				// Pieceworker: Get items sold
 				printf("Enter number of items sold (integer): ");
 				scanf("%d",&quantity);
-				snprintf(emp[c][f++],BUFFER,"%d",quantity);
 
 				// Pieceworker: Get items selling price
 				printf("Enter the price of the item (X.XX): ");
 				scanf("%f",&price);
-				snprintf(emp[c][f++],BUFFER,"%.2f",price);
 
 				// Calculate total and display to user
-				snprintf(emp[c][f],BUFFER,"%.2f",quantity*price);
-				printf(totalPaycheck,emp[c][1],emp[c][4]);
-				c++;
+				emp.total=quantity*price;
+				printf(totalPaycheck,emp.name,emp.total);
+
+				// Add Employee to array
+				addToArray(emp);
 				break;
-			case 5:
+			case 5: // test case
 				printf("Successfully parsed paycode as %d\n",paycode);
 				break;
 			default:
